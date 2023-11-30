3a737280b0fa201dc910a642a77c57acde8464d1
AndriiSuch <suchinets.andriy@gmail.com>
2023-11-30 17:48:07 +0200

    git init

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import ReactDOM from 'react-dom/client';
-import {App} from "./Components/App";
+
+import { Provider } from "react-redux";
+import { store } from "./redux";
+import { router } from "./router";
+import { RouterProvider } from "react-router";
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-
-    <App />
-
+    <Provider store={store}>
+        <RouterProvider router={router} />
+    </Provider>
 );
 
